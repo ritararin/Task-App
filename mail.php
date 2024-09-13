cc5c5626489444a703ca7566fe5d4653965ffe18
ritararin <rita.palmeris@strathmore.edu>
2024-09-13 17:27:59 +0300

    Email Notif added and attempt to start database connection

--- mail.php
+++ mail.php
@@ -7,4 +7,18 @@ function validatingEmail($email) {
     return false;
   }
 }
+
+function sendWelcomeEmail($name, $email) {
+  $subject = 'Welcome to ICS 2.2! Account Verification';
+  $message = 'Hello ' . $name . ',<br><br>You requested an account on ICS 2.2.<br><br>In order to use the account you need to:<br><a href="http://localhost/ics22/verify.php?email=' . $email . '">Click Here</a><br><br> to complete the registration process.<br><br>Regards,<br>Systems Admin<br>ICS 2.2';
+  $headers = 'From: ICS 2.2 <no-reply@example.com>' . "\r\n" .
+    'Reply-To: no-reply@example.com' . "\r\n" .
+    'MIME-Version: 1.0' . "\r\n" .
+    'Content-Type: text/html; charset=UTF-8';
+  if (mail($email, $subject, $message, $headers)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 ?>
